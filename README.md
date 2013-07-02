8264ce334bf56656492abe320b85deb8ca678e4c
jademcosta <jademcosta@gmail.com>
2013-07-01 22:34:23 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,1 @@
+I'm building this to use it as my (computer science) course conclusion project.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjetoFinal::Application.config.secret_token = 'a57d889bd83aaf0ca861e5aa84d7721fb1d72c74a754906136fead094cb4654b72a989cf7bf18db892f208d8de587509e6c73c2695b599779dea74428efe2913'
