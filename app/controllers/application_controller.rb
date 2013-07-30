d874bcb0a30c6172114f11dffcc86cd0c8cdf8f8
jademcosta <jademcosta@gmail.com>
2013-07-29 23:53:57 -0300

    Adicionando metodos e o controler de session (ainda não está ok). Adicionando Página para inscrição de usuários (ainda não ok).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # helper_method :nome_do_metodo
+  # para que o mteodo fique disponivel no application helper ;)
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,4 +8,31 @@ describe "UserPages" do
 
         it { should have_title(full_title('Cadastro')) } 
     end
+
+    describe "signup" do    
+        before { visit signup_path }
+
+        let(:submit) { "Criar conta" }
+
+        describe "with invalid information" do
+            it "should not create a user" do
+                expect { click_button submit }.not_to change(User, :count)
+            end
+        end
+
+        describe "with valid and necessary information" do
+            
+            before do
+                fill_in "Nome", with: "Example user"
+                fill_in "Email", with: "example@email.com"
+                fill_in "Senha", with: "1234"
+                fill_in "Confirme a senha", with: "1234"
+            end
+
+            it "should create a user" do
+                expect { click_button submit }.to change(User, :count).by(1)
+            end
+        end
+    end
+
 end
