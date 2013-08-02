041dd85aad7ee94a4268e799b115cdd0ebdf343b
Jade Costa <jademcosta@gmail.com>
2013-08-01 23:13:38 -0300

    Apenas adicionando futuros arquivos (vazios)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class UsersController < ApplicationController
     def new
         @user = User.new
@@ -8,10 +10,14 @@ class UsersController < ApplicationController
 
 		if @user.save
 			sign_in @user
-			flash[:success] = "Bem vindo a rede de pesquisadores!"
+			flash[:success] = "Bem vindo à rede de pesquisadores!"
 			redirect_to root_path
 		else
 			render 'new'
 		end
+    end
+
+    def show
+        @user = User.find(params[:id])
     end
 end
